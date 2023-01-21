3bd9181dd712417d5a74799e2c103cbcd9a7f051
slavko-vega <s.salontaji@vegait.rs>
2023-01-21 17:00:33 +0100

    added lambda function

--- main.tf
+++ main.tf
@@ -48,3 +48,67 @@ resource "aws_dynamodb_table" "main-data-table" {
     type = "S"
   }
 }
+
+resource "aws_lambda_function" "people_service" {
+  function_name = "PeopleService"
+  role = aws_iam_role.lambda_role.arn
+  handler = "index.handler"
+  runtime = "nodejs16.x"
+  s3_bucket = "arn:aws:s3:::sfcode021-hello-world-artifacts"
+  s3_key = "build.zip"
+  environment {
+    variables = {
+      "DYNAMODB_TABLE" = "main-data-table"
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role_people_service"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_policy_people_service"
+  role = aws_iam_role.lambda_role.id
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "arn:aws:logs:*:*:*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:Query",
+                "dynamodb:Scan",
+                "dynamodb:GetItem",
+                "dynamodb:PutItem",
+                "dynamodb:UpdateItem",
+                "dynamodb:DeleteItem"
+            ],
+            "Resource": "arn:aws:dynamodb:*:*:table/main-data-table"
+        }
+    ]
+}
+EOF
