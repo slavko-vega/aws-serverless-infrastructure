2f4cb2fbaeda23192f181e14cb522cafcc28c9b3
slavko-vega <s.salontaji@vegait.rs>
2023-01-21 17:08:34 +0100

    Ordering and Gateway

--- main.tf
+++ main.tf
@@ -49,22 +49,8 @@ resource "aws_dynamodb_table" "main-data-table" {
   }
 }
 
-resource "aws_lambda_function" "people_service" {
-  function_name = "PeopleService"
-  role = aws_iam_role.lambda_role.arn
-  handler = "index.handler"
-  runtime = "nodejs16.x"
-  s3_bucket = "arn:aws:s3:::sfcode021-hello-world-artifacts"
-  s3_key = "build.zip"
-  environment {
-    variables = {
-      "DYNAMODB_TABLE" = "main-data-table"
-    }
-  }
-}
-
 resource "aws_iam_role" "lambda_role" {
-  name = "lambda_role_people_service"
+  name = "lambda_role_hello_world_service"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -82,7 +68,7 @@ EOF
 }
 
 resource "aws_iam_role_policy" "lambda_policy" {
-  name = "lambda_policy_people_service"
+  name = "lambda_policy_hello_world_service"
   role = aws_iam_role.lambda_role.id
   policy = <<EOF
 {
@@ -112,4 +98,48 @@ resource "aws_iam_role_policy" "lambda_policy" {
     ]
 }
 EOF
+}
+
+resource "aws_lambda_function" "hello_world_service" {
+  function_name = "PeopleService"
+  role = aws_iam_role.lambda_role.arn
+  handler = "index.handler"
+  runtime = "nodejs16.x"
+  s3_bucket = "arn:aws:s3:::sfcode021-hello-world-artifacts"
+  s3_key = "build.zip"
+  environment {
+    variables = {
+      "DYNAMODB_TABLE" = "main-data-table"
+    }
+  }
+}
+
+resource "aws_apigatewayv2_integration" "hello_world" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri    = aws_lambda_function.hello_world_service.arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "hello_world" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.hello_world.id}"
+}
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_world_service.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
